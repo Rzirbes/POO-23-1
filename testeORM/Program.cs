a24f67812c59403033ff970eccfd4ce121c9cd88
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2023-05-10 19:20:32 -0300

    ORM

---
+++ testeORM/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using testeORM.Data.Context;
+using testeORM.Domain.Entities;
+
+Console.WriteLine("Hello, World!");
+
+using var db = new MeuContexto();
+
+//create
+Console.WriteLine("inserting a new person");
+var person = new Person(){Id = 1, Name = "João", Age = DateTime.Now, phoneNumber = "51 9090909"};
+db.Add(person);
+db.SaveChanges();
